58a6842b9c77923411cb912c4ebcc65bac54236f
Horvath Gabor <Gabor.Horvath@eos-hungary.hu>
2019-12-11 13:46:11 +0100

    fixed modified rights kept after changing user or group

--- src/app/rights-grid/rights-grid.component.ts
+++ src/app/rights-grid/rights-grid.component.ts
@@ -59,6 +59,7 @@ export class RightsGridComponent extends GridComponent implements OnInit {
   // Assign the data to the data source for the table to render
   setData(data: RightModel[]): void {
     this.setDataSource(new MatTableDataSource(data))
+    this.modified = []
     data.forEach(right => {
       if (right.allocated === '1') this.selection.select(right)
     })
